bc4cb6e4249fafe363810aacfd4a4f79e59e44eb
Carle <carle.alzieu@gmail.com>
2025-09-17 14:43:14 +0200

    ajout liste

---
+++ main.py
@@ -0,0 +1,16 @@
+# Mastermind #
+
+# Librairies 
+import random
+import os
+
+os.system('cls')
+
+# Red = R
+# Green = G
+# Blue = B
+# Yellow = Y
+# Purple = P 
+# White = W
+
+couleurs = ["R", "G", "B", "Y"]
