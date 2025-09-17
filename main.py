4fc3760dc6ab00329394ad1c2faeab3548a372de
Carle <carle.alzieu@gmail.com>
2025-09-17 21:00:54 +0200

    Ajout correct / partiel + choix joueur

--- main.py
+++ main.py
@@ -13,4 +13,41 @@ os.system('cls')
 # Purple = P 
 # White = W
 
-couleurs = ["R", "G", "B", "Y"]
+
+# Démmarage de la partie
+
+tentative = 0
+listeCouleurs = ["R", "G", "B", "Y", "P", "W"]
+listeRndCouleurs = []
+listeChoixJoueur = []
+winGame = 0
+correct = 0
+partiel = 0
+resultTour = 0
+a=0
+for a in range(5):
+    rndCouleurs = random.choice(listeCouleurs)
+    listeRndCouleurs.append(rndCouleurs)
+    print(listeRndCouleurs)
+
+print("Bienvenue au Mastermind:")
+
+while (winGame == 0 and tentative <=12):
+    
+    b=0
+    c = 0
+    for b in range(5):
+        listeChoixJoueur.append(str(input(f"Couleur {b} : ")))
+        print(listeChoixJoueur)
+
+        if listeChoixJoueur [b] == listeRndCouleurs [b]:
+            correct = correct + 1
+        
+        for c in range(5):
+            if listeChoixJoueur [b] == listeRndCouleurs [c]:
+                partiel = partiel + 1
+    
+    partiel = partiel - correct
+        
+    print(f"Correct {correct}:")
+    print(f"Partiel {partiel}:")
