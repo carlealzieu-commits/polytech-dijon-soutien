335597b8a60a93caaa4ae4f02979874edd38228a
Carle <carle.alzieu@gmail.com>
2025-09-17 23:37:15 +0200

    Fini l'option replay + debug tentative

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ os.system('cls')
 # Démmarage de la partie
 
 # Variables pouvant variés
-tentativeMax = 12
+tentativeMax = 4 
 listeCouleurs = ["R", "G", "B", "Y", "P", "W"]
 rangeCouleurs = 5
 
@@ -28,7 +28,8 @@ listeVerif = []
 winGame = 0
 correct = 0
 partiel = 0
-etatJeu = 0
+play = 0
+nbrTotPartie = 0
 
 a=0
 for a in range(5):
@@ -36,53 +37,66 @@ for a in range(5):
     listeRndCouleurs.append(rndCouleurs)
 # print(listeRndCouleurs) // Utile au debug
 
-print("Bienvenue au Mastermind:")
+print("---------------------------")
+print("  Bienvenue au Mastermind  ")
+print("---------------------------")
+print("")
 
-def demandeJeu ():
+def demandeJeu (play, nbrTotPartie):
     play = 0
     while 1:
         try: 
+            print(f"Nombre de partie réalisé - {nbrTotPartie}")
             avisJoueur = input("Voulez vous jouer ? (Y/N): ")
             if avisJoueur != "Y" and avisJoueur != "N":
                 print(f"Choisir avec 'N' -> Non ou 'Y' -> Yes ")
+                print("")
         except ValueError:
             print(f"Choisir avec 'N' -> Non ou 'Y' -> Yes ")
+            print("")
 
         if avisJoueur == "Y":
             play = 1
+            nbrTotPartie = nbrTotPartie + 1
             print("Préparer vous à jouer ...")
-            print("")
             break
 
         if avisJoueur == "N":
             play = 0
-            print("Dommage, une prochaine fois!")
-            print("")
+            nbrTotPartie = 0
+            print("Dommage, à bientôt!")
             break
         
-    return play
+    return play, nbrTotPartie
 
-etatJeu = demandeJeu()
+play, nbrTotPartie = demandeJeu(play, nbrTotPartie)
+os.system('cls')
 
-while(etatJeu == 1):
-    while (winGame == 0 and tentative <= tentativeMax):
-        tentative = tentative + 1
 
-        print(f"Choisir parmi cette liste de couleur: {listeCouleurs}")
-        
+
+
+while (play == 1): # Jouer plusieurs fois
+    while (winGame == 0 and tentative <= (tentativeMax + 1)): # conditions d'arrêts
+         
+        print("")
+        print(f">> Choisir parmi cette liste de couleur: {listeCouleurs}")
+        print("")
+
         b=0
         c = 0
         for b in range(rangeCouleurs):
             try: 
                 choixJoueur = str(input(f"Couleur {b} : "))
-                if choixJoueur not in listeCouleurs:
+                if choixJoueur not in listeCouleurs and choixJoueur != "":
                     print(f"Choisir parmi cette liste de couleur: {listeCouleurs}")
                     break
             except ValueError:
                 print(f"Choisir parmi cette liste de couleur: {listeCouleurs}")
+                
 
             listeChoixJoueur.append(choixJoueur)
             print(listeChoixJoueur)
+            print("")
 
             # Trouver le nbr de partiel et correct 
             if listeChoixJoueur [b] == listeRndCouleurs [b]:
@@ -100,15 +114,30 @@ while(etatJeu == 1):
         
         print(f"Correct: {correct} | Partiel: {partiel }")
 
+        
+
         if correct == rangeCouleurs:
+            print("")
             print("Vous avez gagné !")
+            print("")
             break
         else:
-            print("Essayer de nouveau")
-            print(f"Encore {tentative} tentative(s)")
-        correct = partiel = 0
+            if tentative <= tentativeMax:
+                print("Essayer de nouveau")
+                print(f"Encore {tentativeMax - tentative} tentative(s)")
+                print("")
+                tentative = tentative + 1
+            else:
+                print("Vous avez perdu :(")
+                print("")
+                break
 
-        etatJeu = demandeJeu()
+        correct = partiel = 0
+        listeChoixJoueur = []
+
+    tentative = 0
+    avisJoueur = 0
+    play, nbrTotPartie = demandeJeu(play, nbrTotPartie)
 
 
 
